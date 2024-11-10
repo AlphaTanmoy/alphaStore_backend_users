30867ec67fcfcf2ce3335b841a63506eeb3569fe
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 10:33:40 +0530

    main file uploaded

--- src/main/kotlin/com/AlphaStore/Users/UsersApplication.kt
+++ src/main/kotlin/com/AlphaStore/Users/UsersApplication.kt
@@ -2,8 +2,10 @@ package com.alphaStore.Users
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class UsersApplication
 
 fun main(args: Array<String>) {
